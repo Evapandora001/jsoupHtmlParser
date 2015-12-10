12af2aba72a5a79da50cce97e0203a22c1086f07
Evapandora001 <evapandora@sina.cn>
2015-12-10 22:00:41 +0800

    2015-12-10

--- src/weiboURL/WeiboWithURL.java
+++ src/weiboURL/WeiboWithURL.java
@@ -58,7 +58,8 @@ public class WeiboWithURL {
 	public static void main(String[] args) {
 		WeiboWithURL a=new WeiboWithURL();
 	//	a.getURL("//FC1//weibo");
-		a.getURL("C://Users//lenovo//Desktop//ApacheSpark");
+//		a.getURL("C://Users//lenovo//Desktop//ApacheSpark");
+		a.getURLThread("//FC1//weibo");
 
 	}
 	
@@ -72,5 +73,68 @@ public class WeiboWithURL {
 		        e.printStackTrace();
 		    }
 	}
+	
+	public void getURLThread(String urlPath){
+		File[] files=new File(urlPath).listFiles();
+		for(File file:files){
+			if(file.getName().endsWith(".dat")==false) continue;
+			System.out.println(file.getName());
+			(new ReadFileThread(file)).start();
+		}
+		
+	}
+	
+	class ReadFileThread extends Thread{
+		private File file;
+		
+		/**
+		 * @return the file
+		 */
+		public File getFile() {
+			return file;
+		}
 
+		/**
+		 * @param file the file to set
+		 */
+		public void setFile(File file) {
+			this.file = file;
+		}
+
+		/**
+		 * @Title:WeiboWithURL.ReadFileThread
+		 * @Description:TODO
+		 */
+		public ReadFileThread(File file) {
+			this.setFile(file);
+		}
+		
+		public void run(){
+			String out="";
+			String outPath="//FC1//weiboWithURL//";
+			int count=0;
+			try {
+			     FileInputStream fis = new FileInputStream(getFile());
+			     InputStreamReader isr = new InputStreamReader(fis,"utf-8");
+			     BufferedReader br = new BufferedReader(isr);
+			     String line = null;
+			     while ((line = br.readLine()) != null) {
+			    	 if(line.split(",")[2].contains("http://t.cn/")==false) continue;
+			    	 out+=line+"\r\n";
+			    	 count++;
+			    	 if(count==10000){
+			    		 writeAppend(out,outPath+getFile().getName());
+			    		 count=0;
+			    		 out="";
+			    	 }
+			        }
+			     writeAppend(out,outPath+getFile().getName());
+			    } catch (Exception e) {
+			        e.printStackTrace();
+			    }		
+		}
+	}
+	
 }
+
+
